bf6290414556e419039e035330aeb9adab4b73b1
23AirJ23 <109483759+23AirJ23@users.noreply.github.com>
2023-04-08 18:23:17 +0300

    Add files via upload

--- lesson_17/js/books.js
+++ lesson_17/js/books.js
@@ -1,8 +1,10 @@
 export default class Book {
-  constructor(author, title, genre, publisher) {
+  constructor(author, title, genre, publisher, availability, origin) {
     this.author = author
     this.title = title
     this.genre = genre
     this.publisher = publisher
+    this.availability = availability
+    this.origin = origin
   }
 }
