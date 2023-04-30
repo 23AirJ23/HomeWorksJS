bad91a3e27005ccc929ab8a9d9950ce24ccec036
23AirJ23 <109483759+23AirJ23@users.noreply.github.com>
2023-04-30 11:53:01 +0300

    Add files via upload

---
+++ lesson_23/js/task_1.js
@@ -0,0 +1,96 @@
+// Завдання 1. Відпустка
+function vacation() {
+  let vacationDays = 56
+  let date = new Date()
+  let workBegins = date.setDate(date.getDate() + vacationDays)
+  let result = new Date(workBegins).toDateString()
+  let vacationDiv = document.querySelector(".vacation")
+  vacationDiv.innerHTML = `На момент поверення дата буде: ${result}`
+}
+
+// Завдання 2. Йогурт
+function yogurt() {
+  let productionYear = parseInt(prompt(`Введіть рік виробництва йогурта`))
+  let productionMonth = parseInt(prompt(`Введіть місяць виробництва йогурта`)) - 1
+  let productionDay = parseInt(prompt(`Введіть день виробництва йогурта`))
+  let expirationDays = 30
+  let productDate = new Date(productionYear, productionMonth, productionDay).getTime()
+  let currentDate = new Date().getTime()
+  let difference = (currentDate - productDate) / 86400000
+  let yogurtDiv = document.querySelector(".yogurt")
+  let pastDedline = Math.abs(difference.toFixed() - expirationDays)
+  if (difference > expirationDays) {
+    yogurtDiv.innerHTML = `Не придатний. Просрочка: ${pastDedline} дні(в)`
+  } else {
+    yogurtDiv.innerHTML = `Придатний. Залишилось ще: ${pastDedline} дні(в)`
+  }
+}
+
+// Завдання 3. Час сортування
+let numbersList = []
+function random() {
+  for (let i = 0; i < 1000; i++) {
+    let rand = Math.floor(Math.random() * 800) + 1
+    numbersList.push(rand)
+  }
+}
+
+function bubbles() {
+  let newArr = Array.from(numbersList)
+  let start = Date.now()
+
+  let changed
+  do {
+    changed = false
+    for (let i = 1; i < newArr.length; i++) {
+      if (newArr[i - 1] > newArr[i]) {
+        let temp = newArr[i - 1]
+        newArr[i - 1] = newArr[i]
+        newArr[i] = temp
+        changed = true
+      }
+    }
+  } while (changed)
+
+  let end = Date.now()
+  let result = end - start
+  let bubblesDiv = document.querySelector(".bubbles")
+  bubblesDiv.innerHTML = `Cортування бульбашкою зайняло: ${result}мс.`
+}
+
+function insert() {
+  let newArr = Array.from(numbersList)
+  let start = Date.now()
+  for (let k = 1; k < newArr.length; k++) {
+    const current = newArr[k]
+    let i = k - 1
+    while (i >= 0 && newArr[i] > current) {
+      newArr[i + 1] = newArr[i]
+      i = i - 1
+    }
+    newArr[i + 1] = current
+  }
+
+  let end = Date.now()
+  let result = end - start
+  let insertDiv = document.querySelector(".insert")
+  insertDiv.innerHTML = `Cортування вставкою зайняло: ${result}мс.`
+}
+
+function additionTask() {
+  let start = Date.now()
+  let question = parseInt(prompt(`Скільки буде 37 + 56`))
+  let end = Date.now()
+  let result = (end - start) / 1000
+  let additionDiv = document.querySelector(".addition")
+  additionDiv.innerHTML = `Ви витратили на додавання: ${result.toFixed(1)}c.`
+}
+
+window.onload = function () {
+  random()
+  bubbles()
+  insert()
+  vacation()
+  yogurt()
+  additionTask()
+}
---
+++ lesson_23/js/task_2.js
@@ -0,0 +1,186 @@
+let arrWithRows = ["Hello", "33 years", "FGHT", "AOUE", "365757", "1 percent"]
+let someString =
+  "45 Це звичайний 4142-3433-2323-3434 рядок 12. В ньому може бути 2459 слів, а може 4642-3833-6723-3634 й 9753безпробілу. А також дата 23.05.2045"
+let govermentUrl = "https://goverment.gov"
+let someyears = "2021 1987 2030 2045 3756 1548"
+let number = "+3809784586572"
+
+// Завдання 1 ------------------------------------------
+function arrayIsNumbers() {
+  let container = document.querySelector(".arrayisnumbers")
+  for (const row of arrWithRows) {
+    if (/\d/.test(row)) {
+      let div = document.createElement("div")
+      div.innerText = row
+      container.append(div)
+    }
+  }
+}
+
+// Завдання 2 ------------------------------------------
+function arrayNotNumber() {
+  let container = document.querySelector(".arraynotnumber")
+  for (const row of arrWithRows) {
+    if (/^[a-zA-Z]+$/.test(row)) {
+      let div = document.createElement("div")
+      div.innerText = row
+      container.append(div)
+    }
+  }
+}
+
+// Завдання 3 ------------------------------------------
+function onlyVowels() {
+  let container = document.querySelector(".onlyvowels")
+  for (const row of arrWithRows) {
+    if (/^[aeiouy]+$/i.test(row)) {
+      let div = document.createElement("div")
+      div.innerText = row
+      container.append(div)
+    }
+  }
+}
+
+// Завдання 4 ------------------------------------------
+function consonants() {
+  let container = document.querySelector(".consonants")
+  for (const row of arrWithRows) {
+    if (/^[qwrtplkjhgfdszxcvbnm]+$/i.test(row)) {
+      let div = document.createElement("div")
+      div.innerText = row
+      container.append(div)
+    }
+  }
+}
+
+// Завдання 5 ------------------------------------------
+function oneOrThree() {
+  let container = document.querySelector(".oneorthree")
+  for (const row of arrWithRows) {
+    if (/[13]/.test(row)) {
+      let div = document.createElement("div")
+      div.innerText = row
+      container.append(div)
+    }
+  }
+}
+
+// Завдання 6 ------------------------------------------
+function stringOnlyNumbers() {
+  let container = document.querySelector(".stringonlynumbers")
+  let num = someString.match(/\s\d{2}\s/g)
+  container.innerHTML = `Лише числа з рядка: ${num}`
+}
+
+// Завдання 7 ------------------------------------------
+function punctuationMarks() {
+  let container = document.querySelector(".punctuationmarks")
+  for (const word of someString) {
+    if (/[.,]/.test(word)) {
+      let div = document.createElement("div")
+      div.innerText = word
+      container.append(div)
+    }
+  }
+}
+
+// Завдання 8 ------------------------------------------
+function separatedByPunctuation() {
+  let container = document.querySelector(".separated-by-punctuation")
+  let sep = someString.match(/[а-їґ]*[.,]\s[а-їґ]*/gi)
+  container.innerHTML = `Розділені розділовими знаками: ${sep}`
+}
+
+// Завдання 9 ------------------------------------------
+function dayMonthYear() {
+  let container = document.querySelector(".day-month-year")
+  let dmy = someString.match(/\d{2}.\d{2}.\d{4}/g)
+  container.innerHTML = `Дата: ${dmy}`
+}
+
+// Завдання 10 ------------------------------------------
+function twoDigitNumbers() {
+  let container = document.querySelector(".two-digit-numbers")
+  let twoDigit = someString.match(/\b\d{2}\b/g)
+  container.innerHTML = `Кількість двоцифрових чисел: ${twoDigit.length}`
+}
+
+// Завдання 11 ------------------------------------------
+function creditCard() {
+  let container = document.querySelector(".credit-card")
+  let credit = someString.match(/\d{4}-\d{4}-\d{4}-\d{4}/g)
+  container.innerHTML = `Номери кредитних карток: ${credit}`
+}
+
+// Завдання 12 ------------------------------------------
+function govermentSite() {
+  let container = document.querySelector(".goverment-site")
+  if (/\bgov$/g.test(govermentUrl)) {
+    container.innerHTML = `Урядовий`
+  } else container.innerHTML = `Не урядовий`
+}
+
+// Завдання 13 ------------------------------------------
+function years() {
+  let container = document.querySelector(".years")
+  let str = someyears.match(/\b\d{4}\b/g)
+  for (const year of str) {
+    if (year > 2021) {
+      let div = document.createElement("div")
+      div.innerText = year
+      container.append(div)
+    }
+  }
+}
+
+// Завдання 14 ------------------------------------------
+function ukraineNum() {
+  let container = document.querySelector(".ukrainenumber")
+  if (/\+38/g.test(number)) {
+    container.innerHTML = `Український`
+  } else container.innerHTML = `Не наш`
+}
+
+// Завдання 15 ------------------------------------------
+function userName() {
+  let container = document.querySelector(".username")
+  let name = prompt(`Введіть прізвище та ім'я через пробіл`)
+  name = name.replace(" ", "-")
+  container.innerHTML = `Ім'я через дефіс: ${name}`
+}
+
+// Завдання 16 ------------------------------------------
+function stringDate() {
+  let container = document.querySelector(".stringdate")
+  let userDate = prompt(`Введіть дату у форматі dd.mm.year`)
+  userDate = userDate.replaceAll(".", "/")
+  container.innerHTML = `Дата у новому форматі: ${userDate}`
+}
+
+// Завдання 17 ------------------------------------------
+function holidayOrNot() {
+  let container = document.querySelector(".holidayornot")
+  let usdrDay = prompt(`Введіть день тижня у форматі номеру дня (0-6) або sun,mon,tue,wed,thu,fri,sat`)
+  if (/[06]/.test(usdrDay) || /s\w{2}/.test(usdrDay)) container.innerHTML = `Вихідний день`
+  else container.innerHTML = `Робочий день`
+}
+
+window.onload = function () {
+  arrayIsNumbers()
+  arrayNotNumber()
+  onlyVowels()
+  consonants()
+  oneOrThree()
+  stringOnlyNumbers()
+  punctuationMarks()
+  separatedByPunctuation()
+  dayMonthYear()
+  twoDigitNumbers()
+  creditCard()
+  govermentSite()
+  years()
+  ukraineNum()
+  userName()
+  stringDate()
+  holidayOrNot()
+}
